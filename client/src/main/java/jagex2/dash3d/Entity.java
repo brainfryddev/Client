6545e8aea2758e641bd7e6bd2ca1db8107abe399
Pazaz <pazaz@itspazaz.com>
2023-04-21 00:47:38 -0400

    chore: Reorganized everything into packages

---
+++ client/src/main/java/jagex2/dash3d/Entity.java
@@ -0,0 +1,26 @@
+package jagex2.dash3d;
+
+import jagex2.client.Signlink;
+import jagex2.datastruct.Linkable;
+import jagex2.graphics.Model;
+import org.openrs2.deob.annotation.OriginalArg;
+import org.openrs2.deob.annotation.OriginalClass;
+import org.openrs2.deob.annotation.OriginalMember;
+import org.openrs2.deob.annotation.Pc;
+
+@OriginalClass("client!w")
+public class Entity extends Linkable {
+
+	@OriginalMember(owner = "client!w", name = "a", descriptor = "(Z)Lclient!eb;")
+	public Model method568(@OriginalArg(0) boolean arg0) {
+		try {
+			if (!arg0) {
+				throw new NullPointerException();
+			}
+			return null;
+		} catch (@Pc(8) RuntimeException local8) {
+			Signlink.reporterror("84732, " + arg0 + ", " + local8.toString());
+			throw new RuntimeException();
+		}
+	}
+}
